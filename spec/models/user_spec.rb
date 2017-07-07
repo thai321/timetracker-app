9a36c07771dafe2663bd23ebe086558872f9e403
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-06 21:55:36 -0700

    implemeted full name for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(
+          email: "test@test.com",
+          first_name: "thai",
+          last_name: "nguyen",
+          password: "123456",
+          password_confirmation: "123456"
+        )
+  end
 
   describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com",
-      first_name: "thai",
-      last_name: "nguyen",
-      password: "123456",
-      password_confirmation: "123456"
-      )
-    end
-
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -20,6 +20,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("NGUYEN, THAI")
     end
   end
 
