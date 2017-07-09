133fbac05b1933b0b4c1e088ea476cf820750194
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-08 18:29:20 -0700

    Implemented custom scope for Posts by users

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
+
+  # get the posts associate with the user
+  scope :post_by, -> (user) { where(user_id: user.id) }
 end
