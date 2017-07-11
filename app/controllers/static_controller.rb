e28410c2c7921d7c24cdd776406f1e7116d43f04
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-10 22:28:12 -0700

    Brought in live data for admin review page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
   def homepage
+    @pending_approvals = Post.includes(:user).where(status: 'submitted')
+    # @pending_approvals = Post.where(status: 'submitted')
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@
 puts "1 User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: @user, overtime_request: 2.5)
+  Post.create!(date: Date.today,
+  rationale: "#{post} rationale content is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  user: @user, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
