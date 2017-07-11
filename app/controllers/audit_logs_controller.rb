f871ae29f73bc89b8c0dd3f463be9ff45457ce41
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-10 19:21:35 -0700

    Blocked non admin users from accessing index page for audits

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -2,5 +2,7 @@ class AuditLogsController < ApplicationController
 
   def index
     @audit_logs = AuditLog.includes(:user)
+    authorize @audit_logs # only admin able to access this index page
   end
+
 end
