472bfee18d7f76e00507111b6089314a4c7af7b1
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-11 15:57:53 -0700

    Implemented callback to update audit log when post is created

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,13 @@ class Post < ApplicationRecord
   # get the posts associate with the user
   # much faster and better query SQL
   scope :post_by, -> (current_user) { includes(:user).where(user_id: current_user.id) }
+
+  after_save :update_audit_log
+
+  private
+  def update_audit_log # where returns an array --> call last
+    audit_log = AuditLog.includes(:user).where(user_id: self.user_id,
+                        start_date: (self.date - 7.days..self.date)).last
+    audit_log.confirmed!
+  end
 end
