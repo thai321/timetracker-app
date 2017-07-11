dcd1531a57c83e8d81593e8fc3c515aa455b3ffe
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-10 23:29:02 -0700

    Added new view elemeetns for audit items that are pending

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,5 +3,6 @@ class StaticController < ApplicationController
     @pending_approvals = Post.includes(:user).where(status: 'submitted')
     # @pending_approvals = Post.where(status: 'submitted')
 
+    @recent_audit_items = AuditLog.last(10)
   end
 end
