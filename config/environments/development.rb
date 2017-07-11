56769a38eb4d90db74bb169c43d0a0017e90743d
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-11 16:10:04 -0700

    Integrated basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
@@ -25,4 +25,8 @@ Rails.application.configure do
 
   # Addition:
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
 end
