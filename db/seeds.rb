0a75d65d00df0476c7f5fecdc6445adacf3679f9
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-11 17:58:12 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 
 puts "1 User created"
 
+
+AuditLog.create!(user: @user, status: 0, start_date: (Date.today - 6.days))
+
+AuditLog.create!(user: @user, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user: @user, status: 0, start_date: (Date.today - 20.days))
+puts "3 auditlogs have been created"
+
 100.times do |post|
   Post.create!(date: Date.today,
   rationale: "#{post} rationale content is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -16,7 +23,6 @@ end
 
 puts "100 Posts have been created"
 
-
 AdminUser.create(email: "admin@admin.com",
                     first_name: "admin",
                     last_name: "admin",
@@ -24,10 +30,3 @@ AdminUser.create(email: "admin@admin.com",
                     password_confirmation: "123456",
                     phone: "4086085237"
                     )
-
-
-AuditLog.create!(user: @user, status: 0, start_date: (Date.today - 6.days))
-
-AuditLog.create!(user: @user, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user: @user, status: 0, start_date: (Date.today - 20.days))
-puts "3 auditlogs have been created"
